9c70b55c04853439588942e56525ce06fcfd5e34
samfeng0517 <sam0880517@gmail.com>
2021-11-02 17:45:31 +0800

    Change svg to png

--- lib/helpers/dataConstants.dart
+++ lib/helpers/dataConstants.dart
@@ -6,14 +6,14 @@ const youtubeLink = 'https://www.youtube.com/';
 const readrProjectLink = 'https://www.readr.tw/';
 
 /// assets
-const String error400Png = 'assets/image/error404.png';
-const String error500Png = 'assets/image/error500.png';
-const String noSignalPng = 'assets/image/noSignal.png';
+const String error400Svg = 'assets/image/error404.svg';
+const String error500Svg = 'assets/image/error500.svg';
+const String noInternetSvg = 'assets/image/noInternet.svg';
 const String logoSvg = 'assets/image/logo.svg';
 const String logoPng = 'assets/image/logo.png';
 const String logoSimplifySvg = 'assets/image/logoSimplify.svg';
 const String defaultImageSvg = 'assets/image/defaultImage.svg';
-const String tabContentNoResultSvg = 'assets/image/tabContentNoResult.svg';
+const String tabNoContentPng = 'assets/image/tabNoContent.png';
 const String homeIconSvg = 'assets/image/homeIcon.svg';
 
 /// color
