b080c3dd1f54b805b140b64fd3a2d1c8b0278226
samfeng0517 <sam0880517@gmail.com>
2021-11-15 16:32:35 +0800

    Change loading skeleton screen to shared

--- lib/pages/home/homeTabContent.dart
+++ lib/pages/home/homeTabContent.dart
@@ -8,8 +8,8 @@ import 'package:readr/models/storyListItemList.dart';
 import 'package:readr/pages/errorPage.dart';
 import 'package:readr/pages/home/homeStoryListItem.dart';
 import 'package:readr/pages/home/homeStoryProjectItem.dart';
+import 'package:readr/pages/shared/storyListSkeletonScreen.dart';
 import 'package:readr/pages/shared/tabContentNoResultWidget.dart';
-import 'package:shimmer/shimmer.dart';
 
 class HomeTabContent extends StatefulWidget {
   final String categorySlug;
@@ -124,59 +124,7 @@ class _HomeTabContentState extends State<HomeTabContent> {
         }
 
         // state is Init, loading, or other
-        return Container(
-          color: Colors.white,
-          child: ListView.builder(
-            padding: const EdgeInsets.only(top: 0),
-            itemCount: 10,
-            itemBuilder: (context, index) {
-              return Shimmer.fromColors(
-                baseColor: const Color.fromRGBO(0, 9, 40, 0.15),
-                highlightColor: const Color.fromRGBO(0, 9, 40, 0.1),
-                child: Container(
-                  margin: const EdgeInsets.fromLTRB(20.0, 24.0, 20.0, 0.0),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(4.0),
-                        child: Container(
-                          height: 90,
-                          width: 90,
-                          color: Colors.white,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 16,
-                      ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              height: 20,
-                              color: Colors.white,
-                            ),
-                            Container(
-                              padding: const EdgeInsets.symmetric(vertical: 6),
-                              width: double.infinity,
-                            ),
-                            Container(
-                              height: 20,
-                              color: Colors.white,
-                              width: (MediaQuery.of(context).size.width - 40) *
-                                  0.52,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          ),
-        );
+        return StoryListSkeletonScreen();
       },
     );
   }
