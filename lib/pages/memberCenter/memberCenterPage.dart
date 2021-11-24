f101b32e8a3283c03f95d5c6d283cd1872d5db19
samfeng0517 <sam0880517@gmail.com>
2021-11-24 12:00:43 +0800

    Rename categoryPage to memberCenterPage

---
+++ lib/pages/memberCenter/memberCenterPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MemberCenterPage extends StatefulWidget {
+  @override
+  _MemberCenterPageState createState() => _MemberCenterPageState();
+}
+
+class _MemberCenterPageState extends State<MemberCenterPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('這是會員中心頁'),
+    );
+  }
+}
