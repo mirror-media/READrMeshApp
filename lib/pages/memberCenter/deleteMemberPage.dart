60b2fff66cd5bbb0e8ec4f67a51b226d15ca84ac
samfeng0517 <sam0880517@gmail.com>
2021-11-25 14:17:53 +0800

    Delete unused package

--- lib/pages/memberCenter/deleteMemberPage.dart
+++ lib/pages/memberCenter/deleteMemberPage.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:readr/helpers/router/router.dart';
 import 'package:readr/models/member.dart';
 
