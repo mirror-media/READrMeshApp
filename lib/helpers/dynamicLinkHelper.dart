86706e916dbfa88439a02ec8713addb0f4eac6f0
samfeng0517 <sam0880517@gmail.com>
2021-11-29 16:44:04 +0800

    Create dynamic link helper

---
+++ lib/helpers/dynamicLinkHelper.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class DynamicLinkHelper {
+  final _auth = FirebaseAuth.instance;
+  void initDynamicLinks() async {
+    FirebaseDynamicLinks.instance.onLink(
+        onSuccess: (PendingDynamicLinkData? dynamicLink) async {
+      final Uri? deepLink = dynamicLink?.link;
+
+      if (deepLink != null) {
+        if (_auth.isSignInWithEmailLink(deepLink.toString())) {
+          _loginWithEmailLink(deepLink.toString());
+        }
+      }
+    }, onError: (OnLinkErrorException e) async {
+      print('onLinkError');
+      print(e.message);
+    });
+
+    final PendingDynamicLinkData? data =
+        await FirebaseDynamicLinks.instance.getInitialLink();
+    final Uri? deepLink = data?.link;
+
+    if (deepLink != null) {
+      if (_auth.isSignInWithEmailLink(deepLink.toString())) {
+        _loginWithEmailLink(deepLink.toString());
+      }
+    }
+  }
+
+  _loginWithEmailLink(String emailLink) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String email = prefs.getString('userEmail') ?? "";
+
+    // The client SDK will parse the code from the link for you.
+    _auth.signInWithEmailLink(email: email, emailLink: emailLink).then((value) {
+      // You can access the new user via value.user
+      // Additional user info profile *not* available via:
+      // value.additionalUserInfo.profile == null
+      // You can check if the user is new or existing:
+      // value.additionalUserInfo.isNewUser;
+
+      print('Successfully signed in with email link!');
+    }).catchError((onError) {
+      print('Error signing in with email link $onError');
+    });
+  }
+}
