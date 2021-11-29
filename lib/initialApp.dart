effd17c818d3f35b177e9d01815f82e1379c846c
samfeng0517 <sam0880517@gmail.com>
2021-11-29 16:42:03 +0800

    Add dynamic link and change init firebase place

--- lib/initialApp.dart
+++ lib/initialApp.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:readr/blocs/categories/bloc.dart';
 import 'package:readr/blocs/config/bloc.dart';
+import 'package:readr/helpers/dynamicLinkHelper.dart';
 import 'package:readr/pages/homePage.dart';
 import 'package:readr/services/categoryService.dart';
 import 'package:readr/services/configService.dart';
@@ -12,6 +13,12 @@ class InitialApp extends StatefulWidget {
 }
 
 class _InitialAppState extends State<InitialApp> {
+  @override
+  void initState() {
+    super.initState();
+    DynamicLinkHelper().initDynamicLinks();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:readr/readrApp.dart';
 
@@ -6,6 +7,6 @@ import 'helpers/environment.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Environment().initConfig(BuildFlavor.staging);
-
+  await Firebase.initializeApp();
   runApp(ReadrApp());
 }
