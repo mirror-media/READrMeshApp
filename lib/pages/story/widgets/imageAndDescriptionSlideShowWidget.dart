733be27771f827a8a58b67dc7af472a03bbe55b3
samfeng0517 <sam0880517@gmail.com>
2021-11-30 16:08:38 +0800

    Delete unused import

--- lib/pages/story/widgets/imageAndDescriptionSlideShowWidget.dart
+++ lib/pages/story/widgets/imageAndDescriptionSlideShowWidget.dart
@@ -1,7 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:readr/helpers/dataConstants.dart';
 import 'package:readr/models/contentList.dart';
 
 class ImageAndDescriptionSlideShowWidget extends StatefulWidget {
