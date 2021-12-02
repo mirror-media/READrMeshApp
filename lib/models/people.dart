1b9090ddf124d49f21e04dc9c90e72ea7e1ea946
samfeng0517 <sam0880517@gmail.com>
2021-12-02 14:57:32 +0800

    Change people model data field

--- lib/models/people.dart
+++ lib/models/people.dart
@@ -1,18 +1,33 @@
 import 'package:readr/models/baseModel.dart';
 
 class People {
-  String? slug;
+  String slug;
   String name;
+  String? bio;
+  String? photoUrl;
 
   People({
     required this.slug,
     required this.name,
+    required this.bio,
+    required this.photoUrl,
   });
 
   factory People.fromJson(Map<String, dynamic> json) {
+    String? bio;
+    if (json['bio'] != null) {
+      bio = json['bio'];
+    }
+    String? photoUrl;
+    if (json['image'] != null) {
+      photoUrl = json['image']['urlMobileSized'];
+    }
+
     return People(
       slug: json[BaseModel.slugKey],
       name: json[BaseModel.nameKey],
+      bio: bio,
+      photoUrl: photoUrl,
     );
   }
 
