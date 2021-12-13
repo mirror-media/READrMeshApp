4b16c82816316f5bf20165c37a58e312b49f0a1c
samfeng0517 <sam0880517@gmail.com>
2021-12-13 17:45:42 +0800

    feat(storyWidget): Add flag check whether to use webView

--- lib/pages/story/storyPage.dart
+++ lib/pages/story/storyPage.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:readr/helpers/environment.dart';
 import 'package:readr/blocs/story/bloc.dart';
@@ -12,8 +13,10 @@ import 'package:readr/pages/story/storyWidget.dart';
 
 class StoryPage extends StatefulWidget {
   final String id;
+  final bool useWebview;
   const StoryPage({
     required this.id,
+    this.useWebview = false,
   });
 
   @override
@@ -42,9 +45,30 @@ class _StoryPageState extends State<StoryPage> {
         body: BlocProvider(
           create: (context) => _bloc,
           child: SafeArea(
-            child: StoryWidget(id: _id),
+            child: widget.useWebview ? _webViewWidget() : StoryWidget(id: _id),
           ),
         ));
+  }
+
+  Widget _webViewWidget() {
+    String url = Environment().config.readrWebsiteLink + 'post/' + _id;
+    InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+        crossPlatform: InAppWebViewOptions(
+          useShouldOverrideUrlLoading: true,
+          mediaPlaybackRequiresUserGesture: false,
+        ),
+        android: AndroidInAppWebViewOptions(
+          useHybridComposition: true,
+        ),
+        ios: IOSInAppWebViewOptions(
+          allowsInlineMediaPlayback: true,
+        ));
+    return InAppWebView(
+      initialUrlRequest: URLRequest(
+        url: Uri.parse(url),
+      ),
+      initialOptions: options,
+    );
   }
 
   PreferredSizeWidget _buildBar(BuildContext context) {
