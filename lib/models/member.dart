fe12d86224fb606bf60d2491586d3f4fef1f1f7e
samfeng0517 <sam0880517@gmail.com>
2021-12-15 17:26:25 +0800

    feat(member_model): Add new object content and fromJson factory

--- lib/models/member.dart
+++ lib/models/member.dart
@@ -1,8 +1,21 @@
 class Member {
-  final String email;
+  final String? email;
   final String firebaseId;
+  final String memberId;
+  final String? nickname;
   Member({
-    required this.email,
     required this.firebaseId,
+    required this.memberId,
+    this.nickname,
+    this.email,
   });
+
+  factory Member.fromJson(Map<String, dynamic> json) {
+    return Member(
+      memberId: json['id'],
+      firebaseId: json['firebaseId'],
+      email: json['email'],
+      nickname: json['nickname'],
+    );
+  }
 }
