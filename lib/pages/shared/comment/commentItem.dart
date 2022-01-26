8b5076c6c7c21a9920a0705bb1ee3ffec01d1e4c
samfeng0517 <sam0880517@gmail.com>
2022-01-26 15:32:22 +0800

    feat: fixed the height when not expand

--- lib/pages/shared/comment/commentItem.dart
+++ lib/pages/shared/comment/commentItem.dart
@@ -96,6 +96,7 @@ class _CommentItemState extends State<CommentItem> {
       color: _backgroundColor,
       padding: const EdgeInsets.all(20),
       width: double.infinity,
+      height: _isExpanded ? null : 105,
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
