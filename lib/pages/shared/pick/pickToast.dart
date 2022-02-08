9cdff320255493f815d097c22d521d4f0d8d101b
samfeng0517 <sam0880517@gmail.com>
2022-02-08 13:23:56 +0800

    feat(pickToast): add bookmark toast

--- lib/pages/shared/pick/pickToast.dart
+++ lib/pages/shared/pick/pickToast.dart
@@ -53,4 +53,56 @@ class PickToast {
       reverseCurve: Curves.linear,
     );
   }
+
+  static void showBookmarkToast(
+      BuildContext context, bool isSuccess, bool isAdd) {
+    String message;
+    if (isAdd) {
+      message = isSuccess ? '成功加入書籤' : '加入書籤失敗';
+    } else {
+      message = isSuccess ? '成功移除書籤' : '移除書籤失敗';
+    }
+    IconData iconData = isSuccess ? Icons.check_circle : Icons.error;
+    Widget toast = Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 7.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(6.0),
+        color: const Color.fromRGBO(0, 9, 40, 0.66),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            iconData,
+            size: 16,
+            color: Colors.white,
+          ),
+          const SizedBox(
+            width: 6.0,
+          ),
+          Text(
+            message,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 13,
+            ),
+          ),
+        ],
+      ),
+    );
+    showToastWidget(
+      toast,
+      context: context,
+      animation: StyledToastAnimation.slideFromTop,
+      reverseAnimation: StyledToastAnimation.slideToTop,
+      position: StyledToastPosition.top,
+      startOffset: const Offset(0.0, -3.0),
+      reverseEndOffset: const Offset(0.0, -3.0),
+      duration: const Duration(seconds: 3),
+      //Animation duration   animDuration * 2 <= duration
+      animDuration: const Duration(milliseconds: 250),
+      curve: Curves.linear,
+      reverseCurve: Curves.linear,
+    );
+  }
 }
