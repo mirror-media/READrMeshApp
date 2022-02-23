3973a3027b176f1b8fd224941f72cd0df1a2bf54
samfeng0517 <sam0880517@gmail.com>
2022-02-23 17:47:30 +0800

    refactor(commentItem): delete useless code

--- lib/pages/shared/comment/commentItem.dart
+++ lib/pages/shared/comment/commentItem.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:auto_route/auto_route.dart';
 import 'package:extended_text/extended_text.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_fadein/flutter_fadein.dart';
 import 'package:readr/helpers/router/router.dart';
@@ -14,7 +15,6 @@ import 'package:readr/services/commentService.dart';
 class CommentItem extends StatefulWidget {
   final Comment comment;
   final Member member;
-  final bool isLiked;
   final bool isExpanded;
   final bool isMyComment;
   final bool isFollowingComment;
@@ -23,7 +23,6 @@ class CommentItem extends StatefulWidget {
   const CommentItem({
     required this.comment,
     required this.member,
-    this.isLiked = false,
     this.isExpanded = false,
     this.isMyComment = false,
     this.isFollowingComment = false,
@@ -65,7 +64,7 @@ class _CommentItemState extends State<CommentItem> {
         _isExpanded = false;
       });
     }
-    _isLiked = widget.isLiked;
+    _isLiked = widget.comment.isLiked;
   }
 
   @override
@@ -134,140 +133,127 @@ class _CommentItemState extends State<CommentItem> {
 
   Widget _nameAndTime(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Flexible(
-          child: Row(
-            children: [
-              Flexible(
-                child: GestureDetector(
-                  onTap: () {
-                    AutoRouter.of(context).push(PersonalFileRoute(
-                        viewMember: widget.comment.member,
-                        currentMember: widget.member));
-                  },
-                  child: Text(
-                    widget.comment.member.nickname,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                      color: Colors.black87,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                width: 2,
-                height: 2,
-                margin: const EdgeInsets.fromLTRB(4.0, 1.0, 4.0, 0.0),
-                alignment: Alignment.center,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.black26,
-                ),
-              ),
-              if (widget.isSending)
-                const Text(
-                  '傳送中',
-                  softWrap: true,
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.black54,
-                  ),
-                ),
-              if (!widget.isSending) Timestamp(widget.comment.publishDate),
-              if (widget.isMyComment) ...[
-                Container(
-                  width: 2,
-                  height: 2,
-                  margin: const EdgeInsets.fromLTRB(4.0, 1.0, 4.0, 0.0),
-                  alignment: Alignment.center,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.black26,
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {},
-                  child: const Text(
-                    '編輯留言',
-                    style: TextStyle(
-                      color: Colors.black54,
-                      fontSize: 12,
-                    ),
-                  ),
-                )
-              ],
-            ],
+        GestureDetector(
+          onTap: () {
+            AutoRouter.of(context).push(PersonalFileRoute(
+                viewMember: widget.comment.member,
+                currentMember: widget.member));
+          },
+          child: Text(
+            widget.comment.member.nickname,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(
+              color: Colors.black87,
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+            ),
           ),
         ),
-        if (!widget.isSending)
-          Flexible(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  widget.comment.likedCount.toString(),
-                  style: const TextStyle(
-                    color: Color.fromRGBO(0, 9, 40, 0.66),
-                    fontSize: 12,
-                  ),
-                ),
-                const SizedBox(width: 5),
-                IconButton(
-                  onPressed: () async {
-                    // save origin state
-                    bool originIsLiked = _isLiked;
-                    int originLikeCount = widget.comment.likedCount;
-                    // refresh UI first
-                    setState(() {
-                      if (_isLiked) {
-                        widget.comment.likedCount--;
-                      } else {
-                        widget.comment.likedCount++;
-                      }
-                      _isLiked = !_isLiked;
-                    });
-                    CommentService commentService = CommentService();
-                    int? newLikeCount;
-                    if (originIsLiked) {
-                      newLikeCount = await commentService.removeLike(
-                        memberId: widget.member.memberId,
-                        commentId: widget.comment.id,
-                      );
-                    } else {
-                      newLikeCount = await commentService.addLike(
-                        memberId: widget.member.memberId,
-                        commentId: widget.comment.id,
-                      );
-                    }
-
-                    // if return null mean failed
-                    if (newLikeCount == null) {
-                      widget.comment.likedCount = originLikeCount;
-                      _isLiked = originIsLiked;
-                    } else {
-                      widget.comment.likedCount = newLikeCount;
-                    }
-                  },
-                  iconSize: 18,
-                  padding: const EdgeInsets.all(0),
-                  constraints: const BoxConstraints(),
-                  icon: Icon(
-                    _isLiked
-                        ? Icons.favorite_outlined
-                        : Icons.favorite_border_outlined,
-                    color: _isLiked
-                        ? Colors.red
-                        : const Color.fromRGBO(0, 9, 40, 0.66),
-                  ),
-                ),
-              ],
+        Container(
+          width: 2,
+          height: 2,
+          margin: const EdgeInsets.fromLTRB(4.0, 1.0, 4.0, 0.0),
+          alignment: Alignment.center,
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.black26,
+          ),
+        ),
+        if (widget.isSending)
+          const Text(
+            '傳送中',
+            softWrap: true,
+            style: TextStyle(
+              fontSize: 12,
+              color: Colors.black54,
             ),
-          )
+          ),
+        if (!widget.isSending) Timestamp(widget.comment.publishDate),
+        if (widget.isMyComment) ...[
+          Container(
+            width: 2,
+            height: 2,
+            margin: const EdgeInsets.fromLTRB(4.0, 1.0, 4.0, 0.0),
+            alignment: Alignment.center,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.black26,
+            ),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: const Text(
+              '編輯留言',
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: 12,
+              ),
+            ),
+          ),
+        ],
+        if (!widget.isSending) ...[
+          const Spacer(),
+          Text(
+            widget.comment.likedCount.toString(),
+            style: const TextStyle(
+              color: Color.fromRGBO(0, 9, 40, 0.66),
+              fontSize: 12,
+            ),
+          ),
+          const SizedBox(width: 5),
+          IconButton(
+            onPressed: () async {
+              if (FirebaseAuth.instance.currentUser != null) {
+                // save origin state
+                bool originIsLiked = _isLiked;
+                int originLikeCount = widget.comment.likedCount;
+                // refresh UI first
+                setState(() {
+                  if (_isLiked) {
+                    widget.comment.likedCount--;
+                  } else {
+                    widget.comment.likedCount++;
+                  }
+                  _isLiked = !_isLiked;
+                });
+                CommentService commentService = CommentService();
+                int? newLikeCount;
+                if (originIsLiked) {
+                  newLikeCount = await commentService.removeLike(
+                    memberId: widget.member.memberId,
+                    commentId: widget.comment.id,
+                  );
+                } else {
+                  newLikeCount = await commentService.addLike(
+                    memberId: widget.member.memberId,
+                    commentId: widget.comment.id,
+                  );
+                }
+
+                // if return null mean failed
+                if (newLikeCount == null) {
+                  widget.comment.likedCount = originLikeCount;
+                  _isLiked = originIsLiked;
+                } else {
+                  widget.comment.likedCount = newLikeCount;
+                }
+              }
+            },
+            iconSize: 18,
+            padding: const EdgeInsets.all(0),
+            constraints: const BoxConstraints(),
+            icon: Icon(
+              _isLiked
+                  ? Icons.favorite_outlined
+                  : Icons.favorite_border_outlined,
+              color:
+                  _isLiked ? Colors.red : const Color.fromRGBO(0, 9, 40, 0.66),
+            ),
+          ),
+        ],
       ],
     );
   }
