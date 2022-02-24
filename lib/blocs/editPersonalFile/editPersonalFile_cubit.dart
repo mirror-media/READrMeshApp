49978209ac69ddc1d8d59f7eeeb93d9920b65477
samfeng0517 <sam0880517@gmail.com>
2022-02-24 12:40:52 +0800

    feat(editPersonalFile_cubit): remove check customId with publisher

--- lib/blocs/editPersonalFile/editPersonalFile_cubit.dart
+++ lib/blocs/editPersonalFile/editPersonalFile_cubit.dart
@@ -27,11 +27,9 @@ class EditPersonalFileCubit extends Cubit<EditPersonalFileState> {
     try {
       List<Publisher> _publisherList =
           await _personalFileService.fetchAllPublishers();
-      int checkResult = _validateNicknameAndId(_publisherList, member);
-      if (checkResult == 1) {
+      bool checkResult = _validateNicknameAndId(_publisherList, member);
+      if (!checkResult) {
         emit(PersonalFileNicknameError());
-      } else if (checkResult == 2) {
-        emit(PersonalFileIdError());
       } else {
         bool? result = await _memberService.updateMember(member);
         if (result == null) {
@@ -47,16 +45,13 @@ class EditPersonalFileCubit extends Cubit<EditPersonalFileState> {
     }
   }
 
-  //return 0 mean pass, 1 mean nickname failed, 2 mean customId failed
-  int _validateNicknameAndId(List<Publisher> publisherList, Member member) {
+  bool _validateNicknameAndId(List<Publisher> publisherList, Member member) {
     for (var publisher in publisherList) {
       if (_equalsIgnoreCase(publisher.title, member.nickname)) {
-        return 1;
-      } else if (_equalsIgnoreCase(publisher.customId!, member.customId)) {
-        return 2;
+        return false;
       }
     }
-    return 0;
+    return true;
   }
 
   bool _equalsIgnoreCase(String string1, String string2) {
