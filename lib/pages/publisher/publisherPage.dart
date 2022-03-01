ce308debb1fe4f08d5f4f0e98a41c9fb73961826
samfeng0517 <sam0880517@gmail.com>
2022-03-01 19:01:25 +0800

    feat: create publisher page and widget

---
+++ lib/pages/publisher/publisherPage.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:readr/blocs/publisher/publisher_cubit.dart';
+import 'package:readr/models/publisher.dart';
+import 'package:readr/pages/publisher/publisherWidget.dart';
+
+class PublisherPage extends StatelessWidget {
+  final Publisher publisher;
+  const PublisherPage(this.publisher);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back_ios_new_outlined,
+            color: Colors.black,
+          ),
+          onPressed: () => Navigator.pop(context),
+        ),
+        automaticallyImplyLeading: false,
+        centerTitle: Platform.isIOS,
+        backgroundColor: Colors.white,
+        title: Text(
+          publisher.title,
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w400,
+            color: Colors.black,
+          ),
+        ),
+        elevation: 0,
+      ),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: BlocProvider(
+          create: (context) => PublisherCubit(),
+          child: PublisherWidget(publisher),
+        ),
+      ),
+    );
+  }
+}
