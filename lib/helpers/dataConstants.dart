2b67b9f0a62c826d0db19a732ba29a9e90e0a9fe
Sam Feng <sam0880517@gmail.com>
2022-03-07 18:51:00 +0800

    refactor: move image folder

--- lib/helpers/dataConstants.dart
+++ lib/helpers/dataConstants.dart
@@ -9,19 +9,19 @@ const readrProjectLink = 'https://www.readr.tw/';
 const String error400Svg = 'assets/image/error404.svg';
 const String error500Svg = 'assets/image/error500.svg';
 const String noInternetSvg = 'assets/image/noInternet.svg';
-const String logoSvg = 'assets/image/logo.svg';
-const String logoPng = 'assets/image/logo.png';
-const String logoSimplifySvg = 'assets/image/logoSimplify.svg';
+const String logoSvg = 'assets/icon/logo.svg';
+const String logoPng = 'assets/icon/logo.png';
+const String logoSimplifySvg = 'assets/icon/logoSimplify.svg';
 const String defaultImageSvg = 'assets/image/defaultImage.svg';
 const String tabNoContentPng = 'assets/image/tabNoContent.png';
-const String homeIconSvg = 'assets/image/homeIcon.svg';
-const String googleLogoSvg = 'assets/image/googleLogo.svg';
+const String homeIconSvg = 'assets/icon/homeIcon.svg';
+const String googleLogoSvg = 'assets/icon/googleLogo.svg';
 const String authorDefaultPng = 'assets/image/authorDefaultImage.png';
-const String commentIconPng = 'assets/image/commentIcon.png';
-const String logoSimplifyPng = 'assets/image/logoSimplify.png';
+const String commentIconPng = 'assets/icon/commentIcon.png';
+const String logoSimplifyPng = 'assets/icon/logoSimplify.png';
 const String noFollowingSvg = 'assets/image/noFollowing.svg';
 const String latestNewsEmptySvg = 'assets/image/latestNewsEmpty.svg';
-const String visitorAvatarPng = 'assets/image/visitorAvatar.png';
+const String visitorAvatarPng = 'assets/icon/visitorAvatar.png';
 
 /// color
 const Color themeColor = Color(0xffFFFFFF);
