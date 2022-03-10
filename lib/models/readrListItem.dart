d9d38e45345220a43faed3b55df6ae5034d05002
Sam Feng <sam0880517@gmail.com>
2022-03-10 19:18:23 +0800

    feat: create readrListItem

---
+++ lib/models/readrListItem.dart
@@ -0,0 +1,7 @@
+import 'package:readr/models/newsListItem.dart';
+
+class ReadrListItem {
+  final NewsListItem newsListItem;
+  final bool isProject;
+  const ReadrListItem(this.newsListItem, this.isProject);
+}
