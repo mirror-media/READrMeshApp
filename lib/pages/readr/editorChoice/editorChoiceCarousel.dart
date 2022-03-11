cb7aee0483468629d4db0ed9dd96360a4e2a5427
Sam Feng <sam0880517@gmail.com>
2022-03-11 17:24:42 +0800

    fix(editorChoiceCarousel): overflowed at bottom

--- lib/pages/readr/editorChoice/editorChoiceCarousel.dart
+++ lib/pages/readr/editorChoice/editorChoiceCarousel.dart
@@ -192,7 +192,7 @@ class _EditorChoiceCarouselState extends State<EditorChoiceCarousel> {
                 _current = index;
               });
             },
-            height: 176,
+            height: 185,
           ),
         ),
         const Divider(
