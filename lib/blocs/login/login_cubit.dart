d24d287b07e9e88f59d9190464177df2bf6ad0c9
Sam Feng <sam0880517@gmail.com>
2022-03-28 15:56:49 +0800

    feat(login_cubit): check keystone whether isNewMember is true or false

--- lib/blocs/login/login_cubit.dart
+++ lib/blocs/login/login_cubit.dart
@@ -14,17 +14,13 @@ class LoginCubit extends Cubit<LoginState> {
   login(bool isNewUser) async {
     emit(Loading());
     try {
-      if (!isNewUser) {
-        var result = await _memberService.fetchMemberData();
-        if (result != null) {
-          await UserHelper.instance.fetchUserData(member: result);
-          await UserHelper.instance.addVisitorFollowing();
-          final prefs = await SharedPreferences.getInstance();
-          await prefs.setBool('isFirstTime', false);
-          emit(ExistingMemberLogin());
-        } else {
-          emit(NewMemberSignup(await _fetchPublisherTitles()));
-        }
+      var result = await _memberService.fetchMemberData();
+      if (result != null) {
+        await UserHelper.instance.fetchUserData(member: result);
+        await UserHelper.instance.addVisitorFollowing();
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.setBool('isFirstTime', false);
+        emit(ExistingMemberLogin());
       } else {
         emit(NewMemberSignup(await _fetchPublisherTitles()));
       }
