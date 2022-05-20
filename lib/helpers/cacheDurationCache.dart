cc0ac13a5eb6419ff52c658ada24a86dac859031
Sam Feng <sam0880517@gmail.com>
2022-05-20 14:09:14 +0800

    feat(cacheDurationCache): increase cache duration

--- lib/helpers/cacheDurationCache.dart
+++ lib/helpers/cacheDurationCache.dart
@@ -3,12 +3,12 @@ const Duration categoryCacheDuration = Duration(hours: 24);
 const Duration newsMarqueeCacheDuration = Duration(minutes: 5);
 const Duration editorChoiceCacheDuration = Duration(minutes: 30);
 const Duration videoEditorChoiceCacheDuration = Duration(minutes: 30);
-const Duration newsTabStoryList = Duration(minutes: 5);
+const Duration newsTabStoryList = Duration(minutes: 30);
 const Duration newsStoryCacheDuration = Duration(minutes: 30);
 
 const Duration showCategoryCacheDuration = Duration(hours: 24);
 const Duration showIntroCacheDuration = Duration(minutes: 30);
-const Duration youtubePlayListCacheDuration = Duration(minutes: 5);
+const Duration youtubePlayListCacheDuration = Duration(minutes: 30);
 
 const Duration anchorPersonListCacheDuration = Duration(hours: 24);
 const Duration anchorPersonCacheDuration = Duration(hours: 24);
