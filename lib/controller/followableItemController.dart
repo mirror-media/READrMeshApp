fd3a60803ee67ac150d98838b95912e5c8a96373
Sam Feng <sam0880517@gmail.com>
2022-05-23 14:11:24 +0800

    feat(followableItemController): update personalFile if isRegistered

--- lib/controller/followableItemController.dart
+++ lib/controller/followableItemController.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:readr/controller/login/choosePublisherController.dart';
+import 'package:readr/controller/personalFile/personalFilePageController.dart';
 import 'package:readr/models/followableItem.dart';
 
 class FollowableItemController extends GetxController {
@@ -28,14 +29,56 @@ class FollowableItemController extends GetxController {
       (callback) {
         if (callback) {
           //update follow publisher count when onbroad
-          if (Get.isRegistered<ChoosePublisherController>()) {
+          if (Get.isRegistered<ChoosePublisherController>() &&
+              item.type == FollowableItemType.publisher) {
             Get.find<ChoosePublisherController>().followedCount.value++;
+          }
+
+          //update own personal file if exists
+          if (Get.isRegistered<PersonalFilePageController>(
+              tag: 'OwnPersonalFile')) {
+            Get.find<PersonalFilePageController>(tag: 'OwnPersonalFile')
+                .followingCount
+                .value++;
+          }
+
+          //update target member personal file if exists
+          if (item.type == FollowableItemType.member &&
+              Get.isRegistered<PersonalFilePageController>(tag: item.id)) {
+            Get.find<PersonalFilePageController>(tag: item.id)
+                .followerCount
+                .value++;
           }
         } else {
           //update follow publisher count when onbroad
           if (Get.isRegistered<ChoosePublisherController>() &&
-              Get.find<ChoosePublisherController>().followedCount.value > 0) {
+              Get.find<ChoosePublisherController>().followedCount.value > 0 &&
+              item.type == FollowableItemType.publisher) {
             Get.find<ChoosePublisherController>().followedCount.value--;
+          }
+
+          //update own personal file if exists
+          if (Get.isRegistered<PersonalFilePageController>(
+                  tag: 'OwnPersonalFile') &&
+              Get.find<PersonalFilePageController>(tag: 'OwnPersonalFile')
+                      .followingCount
+                      .value >
+                  0) {
+            Get.find<PersonalFilePageController>(tag: 'OwnPersonalFile')
+                .followingCount
+                .value--;
+          }
+
+          //update target member personal file if exists
+          if (item.type == FollowableItemType.member &&
+              Get.isRegistered<PersonalFilePageController>(tag: item.id) &&
+              Get.find<PersonalFilePageController>(tag: item.id)
+                      .followerCount
+                      .value >
+                  0) {
+            Get.find<PersonalFilePageController>(tag: item.id)
+                .followerCount
+                .value--;
           }
         }
       },
