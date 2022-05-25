639118d3aefad14664837cf5189f12f677f855bc
Sam Feng <sam0880517@gmail.com>
2022-05-25 18:40:39 +0800

    feat(publisherPage): use getx replace flutter_bloc

---
+++ lib/controller/publisherPageController.dart
@@ -0,0 +1,74 @@
+import 'package:get/get.dart';
+import 'package:readr/helpers/errorHelper.dart';
+import 'package:readr/models/newsListItem.dart';
+import 'package:readr/models/publisher.dart';
+import 'package:readr/services/publisherService.dart';
+
+class PublisherPageController extends GetxController {
+  final PublisherRepos publisherRepos;
+  final Publisher publisher;
+  PublisherPageController({
+    required this.publisherRepos,
+    required this.publisher,
+  });
+
+  final followerCount = 0.obs;
+  final publisherNewsList = <NewsListItem>[].obs;
+  final isLoadingMore = false.obs;
+  final isNoMore = false.obs;
+
+  bool isLoading = true;
+  bool isError = false;
+  dynamic error;
+
+  @override
+  void onInit() {
+    fetchPublisherNews();
+    super.onInit();
+  }
+
+  void fetchPublisherNews() async {
+    isLoading = true;
+    isError = false;
+    update();
+    try {
+      var futureList = await Future.wait([
+        publisherRepos.fetchPublisherNews(publisher.id, DateTime.now()),
+        publisherRepos.fetchPublisherFollowerCount(publisher.id),
+      ]);
+      followerCount.value = futureList[1] as int;
+      publisherNewsList.assignAll(futureList[0] as List<NewsListItem>);
+      if (publisherNewsList.length < 20) {
+        isNoMore.value = true;
+      }
+    } catch (e) {
+      print('Fetch publisher news list error: $e');
+      error = determineException(e);
+    }
+    isLoading = false;
+    update();
+  }
+
+  void fetchMorePublisherNews() async {
+    isLoadingMore.value = true;
+    try {
+      await Future.wait([
+        publisherRepos
+            .fetchPublisherNews(
+                publisher.id, publisherNewsList.last.publishedDate)
+            .then((value) {
+          publisherNewsList.addAll(value);
+          if (value.length < 20) {
+            isNoMore.value = true;
+          }
+        }),
+        publisherRepos
+            .fetchPublisherFollowerCount(publisher.id)
+            .then((value) => followerCount.value = value),
+      ]);
+    } catch (e) {
+      print('Fetch more publisher news list error: $e');
+    }
+    isLoadingMore.value = false;
+  }
+}
--- lib/pages/publisher/publisherPage.dart
+++ lib/pages/publisher/publisherPage.dart
@@ -1,16 +1,23 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:readr/blocs/publisher/publisher_cubit.dart';
+import 'package:get/get.dart';
+import 'package:readr/controller/publisherPageController.dart';
 import 'package:readr/helpers/dataConstants.dart';
+import 'package:readr/models/followableItem.dart';
 import 'package:readr/models/publisher.dart';
-import 'package:readr/pages/publisher/publisherWidget.dart';
+import 'package:readr/pages/errorPage.dart';
+import 'package:readr/pages/shared/followButton.dart';
+import 'package:readr/pages/shared/newsListItemWidget.dart';
+import 'package:readr/pages/shared/publisherLogoWidget.dart';
 import 'package:readr/services/publisherService.dart';
 
-class PublisherPage extends StatelessWidget {
+class PublisherPage extends GetView<PublisherPageController> {
   final Publisher publisher;
   const PublisherPage(this.publisher);
+
+  @override
+  String get tag => publisher.id;
 
   @override
   Widget build(BuildContext context) {
@@ -37,12 +44,143 @@ class PublisherPage extends StatelessWidget {
         elevation: 0,
       ),
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: BlocProvider(
-          create: (context) =>
-              PublisherCubit(publisherRepos: PublisherService()),
-          child: PublisherWidget(publisher),
+      body: GetBuilder<PublisherPageController>(
+        init: PublisherPageController(
+          publisher: publisher,
+          publisherRepos: PublisherService(),
         ),
+        tag: publisher.id,
+        builder: (controller) {
+          if (controller.isError) {
+            return ErrorPage(
+              error: controller.error,
+              onPressed: () => controller.fetchPublisherNews(),
+              hideAppbar: true,
+            );
+          }
+
+          if (!controller.isLoading) {
+            return _buildContent(context);
+          }
+
+          return const Center(
+            child: CircularProgressIndicator.adaptive(),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildContent(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+            border: Border.symmetric(
+              horizontal: BorderSide(
+                color: readrBlack10,
+                width: 0.5,
+              ),
+            ),
+            color: Colors.white,
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 64),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              PublisherLogoWidget(publisher, size: 72),
+              const SizedBox(
+                width: 41.5,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Obx(
+                    () => RichText(
+                      text: TextSpan(
+                        text: _convertNumberToString(
+                          controller.followerCount.value,
+                        ),
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: readrBlack87,
+                        ),
+                        children: const [
+                          TextSpan(
+                            text: ' 人追蹤',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                              color: readrBlack50,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  FollowButton(
+                    PublisherFollowableItem(publisher),
+                    textSize: 16,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: _latestNewsList(context),
+        ),
+      ],
+    );
+  }
+
+  String _convertNumberToString(int number) {
+    if (number >= 10000) {
+      double newNumber = number / 10000;
+      return '${newNumber.toStringAsFixed(newNumber.truncateToDouble() == newNumber ? 0 : 1)}萬';
+    } else {
+      return number.toString();
+    }
+  }
+
+  Widget _latestNewsList(BuildContext context) {
+    return Obx(
+      () => ListView.separated(
+        padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+        itemBuilder: (context, index) {
+          if (index == controller.publisherNewsList.length) {
+            if (controller.isNoMore.isTrue) {
+              return Container();
+            }
+            if (controller.isLoadingMore.isFalse) {
+              controller.fetchMorePublisherNews();
+            }
+            return Container(
+              color: Colors.white,
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: const Center(
+                child: CircularProgressIndicator.adaptive(),
+              ),
+            );
+          }
+          return NewsListItemWidget(
+            controller.publisherNewsList[index],
+          );
+        },
+        separatorBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.only(top: 16, bottom: 20),
+            child: Divider(
+              color: readrBlack10,
+              thickness: 1,
+              height: 1,
+            ),
+          );
+        },
+        itemCount: controller.publisherNewsList.length + 1,
       ),
     );
   }
