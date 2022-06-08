e31ab1bb41e804eacfc63eb631c3c6cd094dccc4
Sam Feng <sam0880517@gmail.com>
2022-06-08 15:37:32 +0800

    feat(collectionAppBar): add tag and inputText alert

--- lib/pages/collection/collectionAppBar.dart
+++ lib/pages/collection/collectionAppBar.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:readr/controller/collection/collectionPageController.dart';
@@ -8,6 +9,9 @@ import 'package:readr/models/collection.dart';
 class CollectionAppBar extends GetView<CollectionPageController> {
   final Collection collection;
   const CollectionAppBar(this.collection);
+
+  @override
+  String get tag => collection.id;
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +25,75 @@ class CollectionAppBar extends GetView<CollectionPageController> {
           Icons.arrow_back_ios_new_outlined,
           color: readrBlack,
         ),
-        onPressed: () => Get.back(),
+        onPressed: () async {
+          if (controller.inputText.trim().isNotEmpty) {
+            Widget dialogTitle = const Text(
+              '確定要刪除留言？',
+              style: TextStyle(
+                color: readrBlack,
+                fontSize: 17,
+                fontWeight: FontWeight.w600,
+              ),
+            );
+            Widget dialogContent = const Text(
+              '系統將不會儲存您剛剛輸入的內容',
+              style: TextStyle(
+                color: readrBlack,
+                fontSize: 13,
+                fontWeight: FontWeight.w400,
+              ),
+            );
+            List<Widget> dialogActions = [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context);
+                },
+                child: const Text(
+                  '刪除留言',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text(
+                  '繼續輸入',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              )
+            ];
+            if (!GetPlatform.isIOS) {
+              await showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: dialogTitle,
+                  content: dialogContent,
+                  buttonPadding: const EdgeInsets.only(left: 32, right: 8),
+                  actions: dialogActions,
+                ),
+              );
+            } else {
+              await showDialog(
+                context: context,
+                builder: (context) => CupertinoAlertDialog(
+                  title: dialogTitle,
+                  content: dialogContent,
+                  actions: dialogActions,
+                ),
+              );
+            }
+          } else {
+            Get.back();
+          }
+        },
       ),
       title: const Text(
         '集錦',
