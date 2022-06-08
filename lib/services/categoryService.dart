972de671f2c8aa131de19a6138e3f7572a91df79
Sam Feng <sam0880517@gmail.com>
2022-06-08 15:04:25 +0800

    fix(readr): category related post may get draft story

--- lib/services/categoryService.dart
+++ lib/services/categoryService.dart
@@ -27,7 +27,13 @@ class CategoryServices implements CategoryRepos {
         id
         name
         slug
-        relatedPost(sortBy: [ publishTime_DESC ], first: 1){
+        relatedPost(
+          where:{
+            state: published
+          }
+          sortBy: [ publishTime_DESC ], 
+          first: 1
+        ){
           publishTime
         }
       }
