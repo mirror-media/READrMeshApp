37d6fff9bd6b687757338052ba6599f5a3da1a25
Sam Feng <sam0880517@gmail.com>
2022-06-10 14:41:26 +0800

    feat(contactUsPage): add discord item

--- lib/pages/setting/contactUsPage.dart
+++ lib/pages/setting/contactUsPage.dart
@@ -56,13 +56,93 @@ class ContactUsPage extends StatelessWidget {
               shrinkWrap: true,
               physics: const NeverScrollableScrollPhysics(),
               children: [
-                _emailItem(context),
+                _buildItem(
+                  title: '客服信箱',
+                  content: 'readr@readr.tw',
+                  onTap: () async {
+                    String currentTime = DateFormat('yyyy/MM/dd HH:mm:ss')
+                        .format(DateTime.now());
+                    EmailContent email = EmailContent(
+                      to: [
+                        'readr@readr.tw',
+                      ],
+                      subject: 'READr Mesh 客服聯絡',
+                      body:
+                          '\n\n\nDate: $currentTime\nApp Version: $appVersion\nPlatform: $platform\nDevice: $device',
+                    );
+                    OpenMailAppResult result =
+                        await OpenMailApp.composeNewEmailInMailApp(
+                            emailContent: email);
+                    if (!result.didOpen && !result.canOpen) {
+                      showNoMailAppsDialog(context);
+                    } else if (!result.didOpen && result.canOpen) {
+                      showCupertinoModalPopup(
+                        context: context,
+                        builder: (context) => CupertinoActionSheet(
+                          actions: [
+                            for (var app in result.options)
+                              CupertinoActionSheetAction(
+                                onPressed: () {
+                                  OpenMailApp.composeNewEmailInSpecificMailApp(
+                                    mailApp: app,
+                                    emailContent: email,
+                                  );
+                                  Navigator.pop(context);
+                                },
+                                child: Text(
+                                  app.name == 'Apple Mail' ? '信件' : app.name,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ),
+                          ],
+                          cancelButton: CupertinoActionSheetAction(
+                            onPressed: () => Navigator.pop(context),
+                            child: const Text(
+                              '取消',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                ),
                 Divider(
                   color: Colors.grey[100],
                   thickness: 1,
                   height: 1,
                 ),
-                _phoneItem(),
+                _buildItem(
+                  title: '客服電話',
+                  content: '(02) 6633-3890',
+                  onTap: () async {
+                    String url = 'tel:02-6633-3890';
+                    if (!await launchUrl(Uri.parse(url))) {
+                      throw 'Could not launch $url';
+                    }
+                  },
+                ),
+                Divider(
+                  color: Colors.grey[100],
+                  thickness: 1,
+                  height: 1,
+                ),
+                _buildItem(
+                  title: 'Discord 社群',
+                  content: 'https://discord.gg/ywpth4mZUw',
+                  onTap: () async {
+                    String url = 'https://discord.gg/ywpth4mZUw';
+                    if (!await launchUrl(Uri.parse(url))) {
+                      throw 'Could not launch $url';
+                    }
+                  },
+                ),
               ],
             ),
           ),
@@ -98,90 +178,6 @@ class ContactUsPage extends StatelessWidget {
     );
   }
 
-  Widget _emailItem(BuildContext context) {
-    return InkWell(
-      onTap: () async {
-        String currentTime =
-            DateFormat('yyyy/MM/dd HH:mm:ss').format(DateTime.now());
-        EmailContent email = EmailContent(
-          to: [
-            'readr@readr.tw',
-          ],
-          subject: 'READr Mesh 客服聯絡',
-          body:
-              '\n\n\nDate: $currentTime\nApp Version: $appVersion\nPlatform: $platform\nDevice: $device',
-        );
-        OpenMailAppResult result =
-            await OpenMailApp.composeNewEmailInMailApp(emailContent: email);
-        if (!result.didOpen && !result.canOpen) {
-          showNoMailAppsDialog(context);
-        } else if (!result.didOpen && result.canOpen) {
-          showCupertinoModalPopup(
-            context: context,
-            builder: (context) => CupertinoActionSheet(
-              actions: [
-                for (var app in result.options)
-                  CupertinoActionSheetAction(
-                    onPressed: () {
-                      OpenMailApp.composeNewEmailInSpecificMailApp(
-                        mailApp: app,
-                        emailContent: email,
-                      );
-                      Navigator.pop(context);
-                    },
-                    child: Text(
-                      app.name == 'Apple Mail' ? '信件' : app.name,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-              ],
-              cancelButton: CupertinoActionSheetAction(
-                onPressed: () => Navigator.pop(context),
-                child: const Text(
-                  '取消',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ),
-          );
-        }
-      },
-      child: Container(
-        color: Colors.white,
-        width: double.maxFinite,
-        padding: const EdgeInsets.symmetric(vertical: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            Text(
-              '客服信箱',
-              style: TextStyle(
-                fontSize: 16,
-                color: readrBlack87,
-              ),
-            ),
-            SizedBox(
-              height: 4,
-            ),
-            Text(
-              'readr@readr.tw',
-              style: TextStyle(
-                fontSize: 16,
-                color: readrBlack50,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   void showNoMailAppsDialog(BuildContext context) {
     showPlatformDialog(
       context: context,
@@ -199,32 +195,33 @@ class ContactUsPage extends StatelessWidget {
     );
   }
 
-  Widget _phoneItem() {
+  Widget _buildItem({
+    required String title,
+    required String content,
+    void Function()? onTap,
+  }) {
     return InkWell(
-      onTap: () async {
-        String url = 'tel:02-6633-3890';
-        if (!await launchUrl(Uri.parse(url))) throw 'Could not launch $url';
-      },
+      onTap: onTap,
       child: Container(
         color: Colors.white,
         width: double.maxFinite,
         padding: const EdgeInsets.symmetric(vertical: 16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
+          children: [
             Text(
-              '客服電話',
-              style: TextStyle(
+              title,
+              style: const TextStyle(
                 fontSize: 16,
                 color: readrBlack87,
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 4,
             ),
             Text(
-              '(02) 6633-3890',
-              style: TextStyle(
+              content,
+              style: const TextStyle(
                 fontSize: 16,
                 color: readrBlack50,
               ),
