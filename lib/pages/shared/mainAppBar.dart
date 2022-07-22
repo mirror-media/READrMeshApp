343771d38659c2a8b90b0c55fefcacc9a1588060
Sam Feng <sam0880517@gmail.com>
2022-07-22 14:49:33 +0800

    feat(mainAppBar): add search icon button

--- lib/pages/shared/mainAppBar.dart
+++ lib/pages/shared/mainAppBar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:readr/controller/mainAppBarController.dart';
@@ -9,6 +10,7 @@ import 'package:readr/getxServices/userService.dart';
 import 'package:readr/helpers/dataConstants.dart';
 import 'package:readr/pages/invitationCode/invitationCodePage.dart';
 import 'package:readr/pages/notify/notifyPage.dart';
+import 'package:readr/pages/search/searchPage.dart';
 
 class MainAppBar extends GetView<MainAppBarController> {
   @override
@@ -22,13 +24,30 @@ class MainAppBar extends GetView<MainAppBarController> {
         appBarIconSvg,
       ),
       actions: [
-        IconButton(
-          onPressed: () => Get.to(() => NotifyPage(), fullscreenDialog: true),
-          icon: SizedBox(
-            width: 30,
-            height: 30,
-            child: Stack(
-              alignment: Alignment.topRight,
+        Container(
+          width: 30,
+          height: 30,
+          margin: const EdgeInsets.symmetric(horizontal: 10.5),
+          child: IconButton(
+            onPressed: () => Get.to(() => SearchPage()),
+            padding: const EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: Icon(
+              PlatformIcons(context).search,
+              size: 26,
+              color: readrBlack87,
+            ),
+          ),
+        ),
+        Container(
+          width: 30,
+          height: 30,
+          margin: const EdgeInsets.symmetric(horizontal: 10.5),
+          child: IconButton(
+            onPressed: () => Get.to(() => NotifyPage(), fullscreenDialog: true),
+            padding: const EdgeInsets.all(0),
+            icon: Stack(
+              alignment: Alignment.center,
               children: [
                 Icon(
                   GetPlatform.isAndroid
@@ -43,7 +62,7 @@ class MainAppBar extends GetView<MainAppBarController> {
                         .unReadNotifyList
                         .isNotEmpty) {
                       return Container(
-                        margin: const EdgeInsets.only(top: 1, right: 2),
+                        margin: const EdgeInsets.only(left: 12, bottom: 12),
                         width: 12,
                         height: 12,
                         decoration: BoxDecoration(
@@ -63,26 +82,28 @@ class MainAppBar extends GetView<MainAppBarController> {
             ),
           ),
         ),
-        Obx(
-          () {
-            if (Get.find<UserService>().isMember.isFalse) {
-              return Container();
-            }
-            return IconButton(
-              onPressed: () {
-                Get.to(
-                  () => InvitationCodePage(),
-                  fullscreenDialog: true,
-                );
-              },
-              icon: SizedBox(
-                width: 30,
-                height: 30,
-                child: Stack(
-                  alignment: Alignment.topRight,
+        Container(
+          width: 30,
+          height: 30,
+          margin: const EdgeInsets.only(left: 9.5, right: 14),
+          child: Obx(
+            () {
+              if (Get.find<UserService>().isMember.isFalse) {
+                return Container();
+              }
+              return IconButton(
+                onPressed: () {
+                  Get.to(
+                    () => InvitationCodePage(),
+                    fullscreenDialog: true,
+                  );
+                },
+                padding: const EdgeInsets.all(0),
+                icon: Stack(
+                  alignment: Alignment.center,
                   children: [
                     const Padding(
-                      padding: EdgeInsets.only(top: 2, right: 4),
+                      padding: EdgeInsets.only(right: 2),
                       child: Icon(
                         CupertinoIcons.envelope,
                         color: readrBlack87,
@@ -95,6 +116,7 @@ class MainAppBar extends GetView<MainAppBarController> {
                           return Container(
                             width: 12,
                             height: 12,
+                            margin: const EdgeInsets.only(left: 18, bottom: 18),
                             decoration: BoxDecoration(
                               color: Colors.red,
                               border: Border.all(
@@ -110,9 +132,9 @@ class MainAppBar extends GetView<MainAppBarController> {
                     ),
                   ],
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
         )
       ],
     );
