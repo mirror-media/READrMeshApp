ad0f0fa481e79d85b9c363bfc8f8d2381aeea763
Sam Feng <sam0880517@gmail.com>
2022-07-22 14:49:06 +0800

    feat(search): create all collection result page

---
+++ lib/pages/search/allCollectionResultPage.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:readr/controller/searchPageController.dart';
+import 'package:readr/helpers/dataConstants.dart';
+import 'package:readr/pages/personalFile/pickCollectionItem.dart';
+
+class AllCollectionResultPage extends GetView<SearchPageController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.5,
+        leading: IconButton(
+          padding: const EdgeInsets.only(left: 16),
+          icon: const Icon(
+            Icons.arrow_back_ios_new_outlined,
+            color: readrBlack,
+          ),
+          onPressed: () => Get.back(),
+        ),
+        centerTitle: true,
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              '包含',
+              style: TextStyle(
+                color: readrBlack,
+                fontSize: 18,
+              ),
+            ),
+            Text(
+              controller.keyWord,
+              style: const TextStyle(
+                color: readrBlack,
+                fontSize: 18,
+              ),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            const Text(
+              '的集錦',
+              style: TextStyle(
+                color: readrBlack,
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: Obx(
+        () => GridView.builder(
+          padding: const EdgeInsets.all(20),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 12,
+            crossAxisSpacing: 12,
+            childAspectRatio: (context.width - 40 - 12) / 500,
+          ),
+          itemBuilder: (context, index) {
+            if (index == controller.collectionResultList.length - 1 &&
+                !controller.isLoadingMoreCollection &&
+                !controller.noMoreCollection) {
+              // controller.loadMoreCollection();
+              print('load more collection');
+            }
+            return PickCollectionItem(
+              controller.collectionResultList[index],
+            );
+          },
+          itemCount: controller.collectionResultList.length,
+        ),
+      ),
+    );
+  }
+}
