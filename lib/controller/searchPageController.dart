90937db57790529f0f197d9da106a4943524cc29
Sam Feng <sam0880517@gmail.com>
2022-07-25 13:44:03 +0800

    feat(searchPageController): split news and colleciton search and add error handling

--- lib/controller/searchPageController.dart
+++ lib/controller/searchPageController.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:meilisearch/meilisearch.dart';
 import 'package:readr/getxServices/environmentService.dart';
 import 'package:readr/getxServices/sharedPreferencesService.dart';
+import 'package:readr/helpers/errorHelper.dart';
 import 'package:readr/models/collection.dart';
 import 'package:readr/models/newsListItem.dart';
 import 'package:readr/services/searchService.dart';
@@ -28,6 +29,8 @@ class SearchPageController extends GetxController {
   final noMoreNews = false.obs;
   bool noMoreCollection = false;
   bool noResult = false;
+
+  dynamic error;
 
   final SearchRepos repos;
   SearchPageController(this.repos);
@@ -67,50 +70,53 @@ class SearchPageController extends GetxController {
     }
     newsResultList.clear();
     collectionResultList.clear();
+    noResult = false;
+    error = null;
     update();
     try {
-      var result = await _index.search(
-        keyWord,
-        attributesToRetrieve: ['id', 'type'],
-      );
+      await Future.wait([
+        _index.search(
+          keyWord,
+          attributesToRetrieve: ['id', 'type'],
+          filter: ['type = story'],
+        ).then((value) async {
+          if (value.hits?.isNotEmpty ?? false) {
+            List<int> newsIdList =
+                List<int>.from((value.hits!).map((e) => e['id']));
+            await repos
+                .fetchNewsByIdList(newsIdList)
+                .then((value) => newsResultList.assignAll(value));
+          }
+        }),
+        _index.search(
+          keyWord,
+          attributesToRetrieve: ['id', 'type'],
+          filter: ['type = collection'],
+        ).then((value) async {
+          if (value.hits?.isNotEmpty ?? false) {
+            List<int> collectionIdList =
+                List<int>.from((value.hits!).map((e) => e['id']));
+            await repos
+                .fetchCollectionsByIdList(collectionIdList)
+                .then((value) => collectionResultList.assignAll(value));
+          }
+        })
+      ]).timeout(const Duration(minutes: 1));
 
-      if (result.hits?.isNotEmpty ?? false) {
-        List<int> newsIdList = [];
-        List<int> collectionIdList = [];
-        for (var item in result.hits!) {
-          newsIdList.addIf(item['type'] == 'story', item['id']);
-          collectionIdList.addIf(item['type'] == 'collection', item['id']);
-        }
-        await Future.wait([
-          repos
-              .fetchNewsByIdList(newsIdList)
-              .then((value) => newsResultList.assignAll(value)),
-          repos
-              .fetchCollectionsByIdList(collectionIdList)
-              .then((value) => collectionResultList.assignAll(value)),
-        ]);
-
-        if (result.hits!.length < 20) {
-          noMoreNews.value = true;
-          noMoreCollection = true;
-        }
-
-        if (newsResultList.isEmpty && collectionResultList.isEmpty) {
-          noResult = true;
-        }
-        noResult = false;
-      } else {
+      if (newsResultList.isEmpty && collectionResultList.isEmpty) {
         noResult = true;
       }
+
+      if (newsResultList.length < 20) {
+        noMoreNews.value = true;
+      }
+
+      if (collectionResultList.length < 20) {
+        noMoreCollection = true;
+      }
     } catch (e) {
+      error = determineException(e);
       print('Search failed: $e');
-      Fluttertoast.showToast(
-        msg: '發生錯誤 請稍後再試',
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.BOTTOM,
-        timeInSecForIosWeb: 1,
-        fontSize: 16.0,
-      );
     }
     isLoading(false);
     update();
@@ -124,6 +130,7 @@ class SearchPageController extends GetxController {
         keyWord,
         attributesToRetrieve: ['id', 'type'],
         offset: newsResultList.length,
+        filter: ['type = story'],
       );
 
       if (result.hits?.isNotEmpty ?? false) {
@@ -160,6 +167,7 @@ class SearchPageController extends GetxController {
         keyWord,
         attributesToRetrieve: ['id', 'type'],
         offset: collectionResultList.length,
+        filter: ['type = collection'],
       );
 
       if (result.hits?.isNotEmpty ?? false) {
