b6d015521adee48409bb8bedf3825fe43ca22976
Sam Feng <sam0880517@gmail.com>
2022-08-09 20:02:04 +0800

    feat(timeDimensionPage): add when change format and add to empty colleciton

--- lib/pages/collection/createAndEdit/timeline/timeDimensionPage.dart
+++ lib/pages/collection/createAndEdit/timeline/timeDimensionPage.dart
@@ -5,25 +5,42 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
 import 'package:readr/controller/collection/createAndEdit/timeDimensionPageController.dart';
 import 'package:readr/helpers/dataConstants.dart';
+import 'package:readr/models/collection.dart';
+import 'package:readr/models/collectionStory.dart';
 import 'package:readr/models/timelineStory.dart';
+import 'package:readr/pages/collection/createAndEdit/chooseFormatPage.dart';
+import 'package:readr/pages/collection/createAndEdit/chooseStoryPage.dart';
 import 'package:readr/pages/collection/createAndEdit/timeline/customTimePage.dart';
 import 'package:readr/pages/collection/shared/timelineItemWidget.dart';
 import 'package:readr/services/collectionService.dart';
 
 class TimeDimensionPage extends GetView<TimeDimensionPageController> {
   final bool isEdit;
+  final bool isChangeFormat;
   final List<TimelineStory> timelineStoryList;
-  const TimeDimensionPage(this.timelineStoryList, {this.isEdit = false});
+  final Collection? collection;
+  final bool isAddToEmpty;
+  const TimeDimensionPage(
+    this.timelineStoryList, {
+    this.isEdit = false,
+    this.isChangeFormat = false,
+    this.isAddToEmpty = false,
+    this.collection,
+  });
 
   @override
   Widget build(BuildContext context) {
     Get.put(TimeDimensionPageController(
       CollectionService(),
       timelineStoryList,
+      collection: collection,
     ));
+    if (isChangeFormat || isAddToEmpty) {
+      controller.hasChange.value = true;
+    }
     return WillPopScope(
       onWillPop: () async {
-        if (controller.hasChange) {
+        if (controller.hasChange.isTrue || isChangeFormat) {
           return await _showLeaveAlertDialog(context) ?? false;
         } else {
           return true;
@@ -43,7 +60,9 @@ class TimeDimensionPage extends GetView<TimeDimensionPageController> {
                   Padding(
                     padding: const EdgeInsets.only(top: 20),
                     child: Text(
-                      isEdit ? '更新集錦中' : '集錦建立中',
+                      (isEdit || isChangeFormat || isAddToEmpty)
+                          ? '更新集錦中'
+                          : '集錦建立中',
                       style: const TextStyle(
                         fontSize: 20,
                         color: readrBlack,
@@ -55,13 +74,13 @@ class TimeDimensionPage extends GetView<TimeDimensionPageController> {
             );
           }
           return Scaffold(
-            backgroundColor: timelineBackgroundColor,
+            backgroundColor: collectionBackgroundColor,
             appBar: AppBar(
               backgroundColor: Colors.white,
               automaticallyImplyLeading: false,
               centerTitle: true,
               elevation: 0.5,
-              leading: isEdit
+              leading: (isEdit || isChangeFormat)
                   ? TextButton(
                       child: const Text(
                         '取消',
@@ -72,7 +91,7 @@ class TimeDimensionPage extends GetView<TimeDimensionPageController> {
                         ),
                       ),
                       onPressed: () async {
-                        if (controller.hasChange) {
+                        if (controller.hasChange.isTrue || isChangeFormat) {
                           await _showLeaveAlertDialog(context);
                         } else {
                           Get.back();
@@ -94,19 +113,122 @@ class TimeDimensionPage extends GetView<TimeDimensionPageController> {
                 ),
               ),
               actions: [
-                TextButton(
-                  child: Text(
-                    isEdit ? '儲存' : '建立',
-                    style: const TextStyle(
-                      color: Colors.blue,
-                      fontSize: 18,
-                    ),
-                  ),
-                  onPressed: () {},
+                Obx(
+                  () {
+                    if (controller.hasChange.isFalse && isEdit) {
+                      return Container();
+                    }
+
+                    return TextButton(
+                      child: Text(
+                        (isEdit || isChangeFormat || isAddToEmpty)
+                            ? '儲存'
+                            : '建立',
+                        style: const TextStyle(
+                          color: Colors.blue,
+                          fontSize: 18,
+                        ),
+                      ),
+                      onPressed: () {
+                        if (isEdit || isAddToEmpty || isChangeFormat) {
+                          controller.updateCollectionPicks(isAddToEmpty);
+                        } else {
+                          controller.createCollection();
+                        }
+                      },
+                    );
+                  },
                 ),
               ],
             ),
             body: _buildBody(context),
+            floatingActionButton: (isEdit || isChangeFormat)
+                ? Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      SizedBox(
+                        width: 40,
+                        height: 40,
+                        child: ElevatedButton(
+                          onPressed: () => Get.to(
+                            () => ChooseFormatPage(
+                              controller.timelineStoryList,
+                              isEdit: true,
+                              initFormat: CollectionFormat.timeline,
+                              collection: collection,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color(0xff04295E),
+                            shadowColor: readrBlack30,
+                            padding: const EdgeInsets.all(12),
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(12.0),
+                              ),
+                            ),
+                            elevation: 6,
+                          ),
+                          child: const Icon(
+                            CupertinoIcons.arrow_right_arrow_left,
+                            color: Colors.white,
+                            size: 13,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      ElevatedButton.icon(
+                        onPressed: () async {
+                          List<CollectionStory> newCollectionStory =
+                              await Get.to(() => ChooseStoryPage(
+                                        isEdit: isEdit || isChangeFormat,
+                                        pickedStoryIds: List<String>.from(
+                                          controller.timelineStoryList.map(
+                                            (element) => element.news.id,
+                                          ),
+                                        ),
+                                      )) ??
+                                  [];
+                          controller.timelineStoryList.addAll(
+                              List<TimelineStory>.from(newCollectionStory.map(
+                                  (e) =>
+                                      TimelineStory.fromCollectionStory(e))));
+                          controller.sortListByTime();
+                        },
+                        style: TextButton.styleFrom(
+                          backgroundColor: readrBlack,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 16,
+                          ),
+                          shadowColor: readrBlack30,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(16.0),
+                            ),
+                          ),
+                          elevation: 6,
+                        ),
+                        icon: const Icon(
+                          CupertinoIcons.add,
+                          size: 17,
+                          color: Colors.white,
+                        ),
+                        label: const Text(
+                          '新增',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                : null,
           );
         },
       ),
@@ -128,7 +250,7 @@ class TimeDimensionPage extends GetView<TimeDimensionPageController> {
                     if (controller.editItemTime) {
                       await _showChangeDimensionAlertDialog(context, value);
                     } else {
-                      controller.updateTimeDimension(value);
+                      controller.updateTimeDimension(value, isEdit);
                     }
                   }
                 },
@@ -283,7 +405,7 @@ class TimeDimensionPage extends GetView<TimeDimensionPageController> {
         actions: [
           PlatformDialogAction(
             onPressed: () {
-              controller.updateTimeDimension(newDimension);
+              controller.updateTimeDimension(newDimension, isEdit);
               controller.sortListByTime();
               controller.editItemTime = false;
               Get.back();
