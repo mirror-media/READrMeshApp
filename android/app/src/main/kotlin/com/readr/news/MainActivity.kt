0dc87ed3ed54b0b19500cf9cfb6983d2b373477c
Sam Feng <sam0880517@gmail.com>
2022-09-20 17:06:24 +0800

    build(android): setup native ads

--- android/app/src/main/kotlin/com/readr/news/MainActivity.kt
+++ android/app/src/main/kotlin/com/readr/news/MainActivity.kt
@@ -1,6 +1,22 @@
 package com.readr.news
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.googlemobileads.GoogleMobileAdsPlugin
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        // TODO: Register the ListTileNativeAdFactory
+        GoogleMobileAdsPlugin.registerNativeAdFactory(
+                flutterEngine, "smallList", SmallListNativeAdFactory(context))
+    }
+
+    override fun cleanUpFlutterEngine(flutterEngine: FlutterEngine) {
+        super.cleanUpFlutterEngine(flutterEngine)
+
+        // TODO: Unregister the ListTileNativeAdFactory
+        GoogleMobileAdsPlugin.unregisterNativeAdFactory(flutterEngine, "smallList")
+    }
 }
