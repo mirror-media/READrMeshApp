f40237cb9b26aba2c8c52a4ca0eb7b4ade526c70
Sam Feng <sam0880517@gmail.com>
2022-09-20 17:06:24 +0800

    build(ios): setup native ads

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import google_mobile_ads
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,11 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+      
+    let smallListFactory = SmallListNativeAdFactory()
+    FLTGoogleMobileAdsPlugin.registerNativeAdFactory(
+        self, factoryId: "smallList", nativeAdFactory: smallListFactory)
+      
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
