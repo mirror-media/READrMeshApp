f37a0aefc3443c8dd04fac8efb5a2590dae3144a
Sam Feng <sam0880517@gmail.com>
2022-11-22 18:11:51 +0800

    feat(getxServices): add walletService

---
+++ lib/getxServices/walletService.dart
@@ -0,0 +1,23 @@
+import 'package:fcl_flutter/fcl_flutter.dart';
+import 'package:get/get.dart';
+import 'package:readr/getxServices/environmentService.dart';
+
+class WalletService extends GetxService {
+  final FclFlutter _fclFlutter = FclFlutter();
+  Future<WalletService> init() async {
+    _fclFlutter.initFCL(
+      Get.find<EnvironmentService>().config.bloctoAppId,
+      useTestNet:
+          Get.find<EnvironmentService>().flavor != BuildFlavor.production,
+    );
+    return this;
+  }
+
+  Future<String?> login() async {
+    return await _fclFlutter.simpleLogin();
+  }
+
+  Future<void> unauthenticate() async {
+    await _fclFlutter.unauthenticate();
+  }
+}
